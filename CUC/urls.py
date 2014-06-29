6bd740aa01736e765f7f330354ad05e1815458c4
Adrien Brunet <ad@ns3100876.ip-91-121-194.eu>
2014-06-29 11:27:16 +0200

    init commit

---
+++ CUC/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
+from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+    )
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'', include('django.contrib.staticfiles.urls')),
+    ) + urlpatterns
